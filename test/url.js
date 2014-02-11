4aa240614c1ec60121dd8993196d8124e41c3504
Evan Hahn <me@evanhahn.com>
2014-02-11 11:34:39 -0500

    cyborg.url

---
+++ test/url.js
@@ -0,0 +1,19 @@
+var robots = require('..');
+require('chai').should();
+
+describe('url', function() {
+
+	it('is found for simple domains', function() {
+		robots.url('http://example.com').should.eql('http://example.com/robots.txt');
+		robots.url('http://example.com/').should.eql('http://example.com/robots.txt');
+		robots.url('http://www.example.com').should.eql('http://www.example.com/robots.txt');
+		robots.url('http://www.example.com/').should.eql('http://www.example.com/robots.txt');
+		robots.url('https://example.com/').should.eql('https://example.com/robots.txt');
+	});
+
+	it('is found for confusing URLs', function() {
+		var url = 'http://user:pASS@hella.dots.host.bike:8080/p/a/t/h?query=string#hash';
+		robots.url(url).should.eql('http://user:pASS@hella.dots.host.bike:8080/robots.txt');
+	});
+
+});

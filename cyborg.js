4e74718ed885f74513ac0be40b7b8be65dac2a77
Evan Hahn <me@evanhahn.com>
2014-02-20 14:36:22 -0500

    parsing user agents with Bots

--- cyborg.js
+++ cyborg.js
@@ -1,5 +1,10 @@
 module.exports = {
+
 	build: require('./lib/build'),
 	middleware: require('./lib/middleware'),
-	url: require('./lib/url')
+	Bot: require('./lib/bot/index'),
+	url: require('./lib/url'),
+
+	DEFAULT: require('./lib/default')
+
 };
